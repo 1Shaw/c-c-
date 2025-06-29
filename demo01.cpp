d883718bf46c364922a0d36fb669d27d92f2f269
1Shaw <xifeng1115@163.com>
2025-06-29 21:34:14 +0800

    Add files via upload

---
+++ demo01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello, world!" << endl;
+    cout<< 1+2 << endl; // This line outputs the result of 1 + 2
+    return 0;
+    
+}
+
+// C++ compiler version: This project uses the compiler version.
